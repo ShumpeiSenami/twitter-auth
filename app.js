858dfa737448ed948079f5ee86c3a82e6534bc6b
ShumpeiSenami <kaifenan@gmail.com>
2019-02-25 05:02:10 +0000

    github認証を追加

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
 var session = require('express-session');
-var TwitterStrategy = require('passport-twitter').Strategy;
 var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var GitHubStrategy = require('passport-github2').Strategy;
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 app.use (helmet());
 
 // view engine setup
@@ -25,19 +26,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ユーザー情報をセッションに保存するために初期化
+// ユーザー情報をセッションに保存するために初期化 (twitter,github 共通)
 app.use(session({
   secret: 'secrete-key',
   resave: true,
   saveUninitialized: true
 }));
 
-// passport自体の初期化
+// passport自体の初期化 (twitter,github 共通)
 app.use(session({ secret:'41364e69455ebbdc', resave: false, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// passport-twitterの設定
+// passport-twitterの設定 (twitter認証のためのもの)
 passport.use(new TwitterStrategy({
   consumerKey:'k5ybd8wmIKyfD2QH7NHNW7nFT',
   consumerSecret: 'keMHKXgpwntczuOniNuuXREPHLj8lc0tWCGVYaEFBQBDL2Puig',
@@ -48,6 +49,23 @@ function(token, tokenSecret, profile, done){
   return done(null, profile);
 }
 ));
+
+// passport-githubの設定
+var GITHUB_CLIENT_ID = 'b8f5aebffc72210cea55';
+var GITHUB_CLIENT_SECRET = 'c6b4d59fcd0ff616a9366aa987ed607f83c74ae5';
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  function(accessToken, refreshToken, profile, done){
+    process.nextTick(function(){
+      return done(null, profile);
+    });
+  }
+  ));
+
 // セッションに保存
 passport.serializeUser(function(user, done){
   done(null, user);
@@ -61,15 +79,26 @@ passport.deserializeUser(function(obj, done){
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// twitterへの認証を実行した際の動作(twitter認証のためのもの)
 app.get('/auth/twitter',
   passport.authenticate('twitter'),
   function(req, res){
   });
-
 app.get('/auth/twitter/callback',
   passport.authenticate('twitter', { failureRedirect: 'auth_failed' }),
   function (req, res) {
     res.redirect('/');
+  });
+// githubへの認証を実行した際の動作（github認証のためのもの）
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function(req, res){
+  });
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res){
+    res.redirect('/')
   });
 
   app.get('/login', function(req, res){
